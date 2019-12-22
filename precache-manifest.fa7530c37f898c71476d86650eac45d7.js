3894b9742b0ffc5d141f7a68233e2fb7f10bd98e
Hannes O <hannesottt@gmail.com>
2019-12-23 01:56:48 +0200

    Updates

---
+++ precache-manifest.fa7530c37f898c71476d86650eac45d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a85e9cfa9a19b3f742d4c72f9ef5ab",
+    "url": "/react-bmi-calculator/index.html"
+  },
+  {
+    "revision": "c642ce8ae8fe752951a2",
+    "url": "/react-bmi-calculator/static/css/2.e315d21c.chunk.css"
+  },
+  {
+    "revision": "040d412bf7e917fe395e",
+    "url": "/react-bmi-calculator/static/css/main.5760a1e5.chunk.css"
+  },
+  {
+    "revision": "c642ce8ae8fe752951a2",
+    "url": "/react-bmi-calculator/static/js/2.90753faf.chunk.js"
+  },
+  {
+    "revision": "63724463194daffb577a73b432f58432",
+    "url": "/react-bmi-calculator/static/js/2.90753faf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "040d412bf7e917fe395e",
+    "url": "/react-bmi-calculator/static/js/main.29f1bc77.chunk.js"
+  },
+  {
+    "revision": "65f77bf9db44c7043035",
+    "url": "/react-bmi-calculator/static/js/runtime-main.147bd3d1.js"
+  }
+]);
